4ea59b28664f1b6e10caa9727d5bf7d701c64afb
tuki9 <motojorvv@themis.ocn.ne.jp>
2023-12-13 22:01:55 +0900

    lesson9課題

---
+++ src/lesson9/kadai/Review05.java
@@ -0,0 +1,104 @@
+package lesson9.kadai;
+
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.PreparedStatement;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+
+        public class Review05 {
+
+
+        public static void main(String[] args) {
+                // 3. データベース接続と結果取得のための変数宣言
+                Connection con = null;
+                PreparedStatement pstmt = null;
+                ResultSet rs = null;
+
+                try {
+                    // 1. ドライバのクラスをJava上で読み込む
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    // 2. DBと接続する
+                    con = DriverManager.getConnection(
+                            "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+                            "root",
+                            "24Oshigoto1113"
+                        );
+                    // 4. DBとやりとりする窓口（Statementオブジェクト）の作成
+                    String sql = "select * from person where Id = ?" ;
+                    pstmt = con.prepareStatement(sql);
+
+                    // 5, 6. Select文の実行と結果を格納／代入
+                    System.out.print("検索キーワードを入力してください > ");
+                    int input = Integer.parseInt(keyIn());
+
+                    // PreparedStatementオブジェクトの?に値をセット
+                            pstmt.setInt(1, input);
+                            rs = pstmt.executeQuery();
+
+
+                    // 7. 結果を表示する
+                    while( rs.next() ){
+                        // Name列の値を取得
+                        String name = rs.getString("Name");
+                        // 年齢列の値を取得
+                        int age = rs.getInt("age");
+
+
+                        // 取得した値を表示
+                        System.out.println(name);
+                        System.out.println(age);
+
+                    }
+                } catch (ClassNotFoundException e) {
+                    System.err.println("JDBCドライバのロードに失敗しました。");
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    System.err.println("データベースに異常が発生しました。");
+                    e.printStackTrace();
+                } finally {
+                    // 8. 接続を閉じる
+                    if( rs != null ){
+                        try {
+                            rs.close();
+                        } catch (SQLException e) {
+                            System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                            e.printStackTrace();
+                        }
+                    }
+                    if( pstmt != null ){
+                        try {
+                            pstmt.close();
+                        } catch (SQLException e) {
+                            System.err.println("Statementを閉じるときにエラーが発生しました。");
+                            e.printStackTrace();
+                        }
+                    }
+                    if(con != null) {
+                        try {
+                            con.close();
+                        } catch (SQLException e) {
+                            System.err.println("データベース切断時にエラーが発生しました。");
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        private static String keyIn() {
+            String line = null;
+
+            try {
+                BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+                line = key.readLine();
+            } catch (IOException e) {
+
+            }
+            return line;
+        }
+
+        }
+
